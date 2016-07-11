a67ee872e8fba2d04f1fe38477c12d9bda4050c7
Snehil Modani <snehil@actonate.com>
2016-07-11 15:36:13 +0530

    Added Base and Extended separate builds, dist added into repo

--- gulp/config.js
+++ gulp/config.js
@@ -1,22 +1,49 @@
 var args = require('minimist')(process.argv.slice(2));
-var VERSION = args.version || require('../package.json').version;
+var VERSION = args.version || require('../package.json')
+  .version;
 
 module.exports = {
-  banner:
-  '/*!\n' +
-  ' * Angular Material Design\n' +
-  ' * https://github.com/angular/material\n' +
-  ' * @license MIT\n' +
-  ' * v' + VERSION + '\n' +
-  ' */\n',
+  banner: '/*!\n' +
+    ' * Angular Material Design\n' +
+    ' * https://github.com/angular/material\n' +
+    ' * @license MIT\n' +
+    ' * v' + VERSION + '\n' +
+    ' */\n',
   jsBaseFiles: [
     'src/core/**/*.js'
   ],
   jsFiles: [
-    'src/**/*.js',
+    'src/core/**/*.js',
+    'src/components/backdrop/**/*.js',
+    'src/components/button/**/*.js',
+    'src/components/card/**/*.js',
+    'src/components/checkbox/**/*.js',
+    'src/components/colors/**/*.js',
+    'src/components/content/**/*.js',
+    'src/components/icon/**/*.js',
+    'src/components/input/**/*.js',
+    'src/components/menu/**/*.js',
+    'src/components/progressCircular/**/*.js',
+    'src/components/radioButton/**/*.js',
+    'src/components/select/**/*.js',
+    'src/components/showHide/**/*.js',
+    'src/components/swipe/**/*.js',
+    'src/components/tabs/**/*.js',
+    'src/components/toast/**/*.js',
     '!src/**/*.spec.js'
   ],
-  mockFiles : [
+  jsExtendedFiles: [
+    'src/components/datePicker/**/*.js',
+    'src/components/dialog/**/*.js',
+    'src/components/gridList/**/*.js',
+    'src/components/navBar/**/*.js',
+    'src/components/processLinear/**/*.js',
+    'src/components/sidenav/**/*.js',
+    'src/components/tooltip/**/*.js',
+    'src/components/virtualRepeat/**/*.js',
+    '!src/**/*.spec.js'
+  ],
+  mockFiles: [
     'test/angular-material-mocks.js'
   ],
   themeBaseFiles: [
@@ -39,18 +66,42 @@ module.exports = {
     'src/core/services/layout/layout.scss'
   ],
   scssLayoutAttributeFiles: [
-      'src/core/style/variables.scss',
-      'src/core/style/mixins.scss',
-      'src/core/services/layout/layout-attributes.scss'
-    ],
-  scssPaths : [
-    'src/components/**/*.scss',
+    'src/core/style/variables.scss',
+    'src/core/style/mixins.scss',
+    'src/core/services/layout/layout-attributes.scss'
+  ],
+  scssPaths: [
+    'src/components/card/**/*.scss',
+    'src/components/backdrop/**/*.scss',
+    'src/components/button/**/*.scss',
+    'src/components/checkbox/**/*.scss',
+    'src/components/colors/**/*.scss',
+    'src/components/content/**/*.scss',
+    'src/components/icon/**/*.scss',
+    'src/components/input/**/*.scss',
+    'src/components/menu/**/*.scss',
+    'src/components/progressCircular/**/*.scss',
+    'src/components/radioButton/**/*.scss',
+    'src/components/select/**/*.scss',
+    'src/components/showHide/**/*.scss',
+    'src/components/swipe/**/*.scss',
+    'src/components/switch/**/*.scss',
+    'src/components/tabs/**/*.scss',
+    'src/components/toast/**/*.scss',
     'src/core/services/**/*.scss'
   ],
-  cssIEPaths : ['src/**/ie_fixes.css'],
+  scssExtendedPaths: [
+    'src/components/datePicker/**/*.scss',
+    'src/components/dialog/**/*.scss',
+    'src/components/gridList/**/*.scss',
+    'src/components/navBar/**/*.scss',
+    'src/components/processLinear/**/*.scss',
+    'src/components/sidenav/**/*.scss',
+    'src/components/tooltip/**/*.scss',
+    'src/components/virtualRepeat/**/*.scss'
+  ],
+  cssIEPaths: ['src/**/ie_fixes.css'],
   paths: 'src/{components, services}/**',
   outputDir: 'dist/',
   demoFolder: 'demo-partials'
 };
-
-
